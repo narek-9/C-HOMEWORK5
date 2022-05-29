91acdee3a031226748b877e4d8d37ed3f733fced
Narek <narekazibekyan5@gmail.com>
2022-05-29 11:37:45 +0400

    Task_36

---
+++ Task_36/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задайте одномерный массив, заполненный
+// случайными числами. Найдите сумму элементов, стоящих
+// на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] array = new int[new Random().Next(5, 16)];
+var newRnd = new Random();
+int sum = 0;
+Console.Write("[");
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = newRnd.Next(0, 1000);
+    Console.Write(array[i] + ", ");
+
+    if(i % 2 != 0)
+    {
+        sum = sum + array[i];
+    }
+
+}
+
+Console.WriteLine("]");
+Console.WriteLine($"Сумма чисел, стоящих на нечетных позициях = {sum}.");
